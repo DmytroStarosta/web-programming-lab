405ddc86a6e02e6551e215e0a1200f8f276ba3f1
DmytroStarosta <151664402+DmytroStarosta@users.noreply.github.com>
2024-11-29 10:09:39 +0200

    Add lab_10

--- lab_10/backend/app.js
+++ lab_10/backend/app.js
@@ -40,7 +40,7 @@ app.get('/trees', (req, res) => {
     params.push(Number(priceMax));
   }
 
-  console.log("SQL Query: ", sql);
+  console.log("SQL Query: ", sql)
   console.log("SQL Parameters: ", params);
 
   db.query(sql, params, (error, results) => {
@@ -50,11 +50,11 @@ app.get('/trees', (req, res) => {
     }
     console.log("Fetched Data from DB: ", results);
 
-    const updatedResults = results.map((tree) => ({
-      ...tree,
-      imageUrl: `/images/${tree.id}.jpg`,
-    }));
-
-    res.status(200).json(updatedResults);
+    res.status(200).json(JSON.parse(JSON.stringify(results)));
   });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
